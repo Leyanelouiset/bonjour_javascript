21ac24c993af0038b2cf8a0dfd24a58eb088a4dc
leyanelouiset <louisetleyane@gmail.com>
2024-10-16 12:11:36 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+/*let message = "bonjour!"
+console.log(message)
+
+let firstname = "beyonce"
+
+function fusion(first,name){
+    console.log(first,name)
+}
+
+fusion(message + firstname */
+
+
+//rendre//
+
+/*let message = "Bonjour!"
+console.log(message)
+
+let firstname = "beyonce"
+
+message = "Bonjour " + firstname + " !"
+
+console.log(message)/*/
+
+//
+
+let message = "Bonjour!"
+
+console.log(message)
+
+let firstname = "beyonce"
+
+firstname = "beyonce"+" et leyane"
+
+message = "Bonjour " + firstname + " !"
+
+console.log(message)
+    
+let secondname = " nico et rodic"
+
+console.log(secondname)
+
+message=" bonjour "+firstname+ secondname+ " sont les meilleurs en dev !"
+console.log(message) 
